fd9f20eee21ada3113209a8462ac7b407dbf43ce
AMIQ Consulting <office@amiq.com>
2020-09-18 18:03:12 +0300

    Update amiq_reg_agent.svh

--- amiq_reg_agent/amiq_reg_agent.svh
+++ amiq_reg_agent/amiq_reg_agent.svh
@@ -1,12 +1,22 @@
 /******************************************************************************
- * (C) Copyright Amiq 2019 All Rights Reserved
+ * (C) Copyright 2020 AMIQ Consulting
  *
- * MODULE:
- * AUTHOR: marghe
- * DATE: Nov 15, 2019
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * FILE DESCRIPTION:
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * NAME:        amiq_reg_agent.svh
+ * PROJECT:     amiq_reg_agent
+ * Description: The agent class containing instances of register block, sequencer
+                config object and coverage collector
  *******************************************************************************/
 class amiq_reg_agent extends uvm_agent;
 	`uvm_component_utils(amiq_reg_agent)
