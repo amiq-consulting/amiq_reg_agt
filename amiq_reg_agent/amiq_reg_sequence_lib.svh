b1fb62adaaff94712430e0191294ae425b1629e7
AMIQ Consulting <office@amiq.com>
2020-09-18 18:14:30 +0300

    Update amiq_reg_sequence_lib.svh

--- amiq_reg_agent/amiq_reg_sequence_lib.svh
+++ amiq_reg_agent/amiq_reg_sequence_lib.svh
@@ -1,3 +1,23 @@
+/******************************************************************************
+ * (C) Copyright 2020 AMIQ Consulting
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * NAME:        amiq_reg_sequence_lib.svh
+ * PROJECT:     amiq_reg_agent
+ * Description: The main sequences provided by the Register Agent
+ *******************************************************************************/
+
 class amiq_reg_base_sequence extends uvm_sequence;
 	`uvm_object_utils(amiq_reg_base_sequence)
 	`uvm_declare_p_sequencer(amiq_reg_sequencer)
